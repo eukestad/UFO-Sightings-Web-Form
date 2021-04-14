75e8d88b91cbe50a011c6d5e4c4247173ad1c3ee
eukestad <69314608+eukestad@users.noreply.github.com>
2021-04-13 23:55:12 -0500

    saving date filter

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,14 +1,63 @@
 // from data.js
 var tableData = data;
 
-// Get a reference to the table body
-var tbody = d3.select("tbody");
-
-data.forEach((ufoReport) => {
-    console.log('It worked!')
+function init() {
+  tableData.forEach((ufoReport) => {
+    // console.log(Object.keys(ufoReport))
     var row = tbody.append("tr");
     Object.entries(ufoReport).forEach(([key, value]) => {
       var cell = row.append("td");
       cell.text(value);
     });
   });
+}
+
+// Get a reference to the table body
+var tbody = d3.select("tbody");
+
+// Select the button
+var button = d3.select("#filter-btn");
+// Select the form
+var form = d3.select("#form");
+
+// Create event handlers 
+button.on("click", runEnter);
+form.on("submit",runEnter);
+
+// Complete the event handler function for the form
+function runEnter() {
+  // Prevent the page from refreshing
+  d3.event.preventDefault();
+
+  var inputElement = d3.select("#datetime");
+  var inputDate = inputElement.property("value");
+  console.log(inputDate);
+
+  if (inputDate != "") {
+    // Remove current rows
+    tbody.selectAll("tr").remove()
+
+    // Re-populate Table
+    tableData.forEach((ufoReport) => {
+      console.log('Date Entered: ', inputDate);
+      if (ufoReport.datetime === inputDate) {
+        var row = tbody.append("tr");
+        Object.entries(ufoReport).forEach(([key, value]) => {
+          var cell = row.append("td");
+          cell.text(value);
+        });
+      }
+
+    });
+  }
+
+  else {
+    // Remove current rows
+    tbody.selectAll("tr").remove()
+    
+    // Re-populate Table
+    init()
+  };
+};
+
+init();
